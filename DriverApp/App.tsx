2227f4497a274110e7842d788c9f17af5b4688ef
20uon0073 <20uon0073@uon.edu.pk>
2024-01-07 13:25:59 +0500

    driver mutations updated

--- DriverApp/App.tsx
+++ DriverApp/App.tsx
@@ -8,29 +8,51 @@
 import React,{useEffect} from 'react';
 import { SafeAreaView, StatusBar } from 'react-native';
 import HomeScreen from './src/screens/HomeScreen';
-import  Amplify,{
-  Auth,
-  API,
-  graphqlOperation,
-}  from 'aws-amplify';
-import config from './src/aws-exports';
+import { API, Amplify, Auth ,graphqlOperation} from 'aws-amplify';
+import awsconfig from './src/aws-exports';
 import { withAuthenticator } from 'aws-amplify-react-native';
 import { getCarId } from './src/graphql/queries';
+import { createCar } from './src/graphql/mutations';
 
 
-Amplify.configure(config);
+Amplify.configure(awsconfig);
 
 const App: React.FC = () => {
 
   useEffect(()=>{
     const updateUserCar=async()=>{
-      // get Authenicated User
+      // get authenticated User
+
+      const authenticatedUser=await Auth.currentAuthenticatedUser({bypassCache:true});
+      if (!authenticatedUser){
+        return;
+      }
 
 
       // check if the user has already a car 
+        const carData=await API.graphql(
+          graphqlOperation(
+            getCarId,
+            {id: authenticatedUser.attributes.sub }
+          )
+        )
 
-
+        if (!!carData.data.getCar){
+          console.log("user already has a car assigned ");
+          return;
+        }
+ 
       // if not ,create a new car for the user
+
+      const newCar={
+        id:authenticatedUser.attributes.sub,
+        type:'RidyX',
+        userId:authenticatedUser.attributes.sub,
+      }
+      await API.graphql(graphqlOperation(
+        createCar,{input:newCar}
+
+      ))
 
     };
 
