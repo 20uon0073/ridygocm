30bdb100d31d60326f9884752c1d0b8cbc79b049
20uon0073 <20uon0073@uon.edu.pk>
2024-01-13 16:40:05 +0500

    cd git vadm  driver Home index

--- DriverApp/src/screens/HomeScreen/index.js
+++ DriverApp/src/screens/HomeScreen/index.js
@@ -21,216 +21,181 @@ const destination = {latitude: 32.101606, longitude: 74.860951};
 const GOOGLE_MAPS_APIKEY = 'AIzaSyDeJpCLAWlRgFul8TvXGiLP8lp1p5Mf0HQ';
 
 const HomeScreen = () => {
-  const [car,setCar]=useState(null);
-   const [myPosition, setMyPosition ]=useState(null);
+  const [car, setCar] = useState(null);
+  const [myPosition, setMyPosition] = useState(null);
+  const [order, setOrder] = useState(null)
+  const [newOrders, setNewOrders] = useState([]);
 
-   const [order, setOrder]=useState(null)
-   const [newOrders,setNewOrders]=useState([]);
-
-
-   const fetchCar = async () => {
+  const fetchCar = async () => {
     try {
       const userData = await Auth.currentAuthenticatedUser();
       const carData = await API.graphql(
-        graphqlOperation(getCar,{id:userData.attributes.sub}),
-      )
+        graphqlOperation(getCar, { id: userData.attributes.sub }),
+      );
       setCar(carData.data.getCar);
-  
     } catch (e) {
       console.error(e);
     }
-  };
+  }
 
-  const fetchOrders =async()=>{
+  const fetchOrders = async () => {
     try {
-   const  ordersData =await API.graphql(
-     graphqlOperation(
-      listOrders,
-        { filter: { status: { eq: 'NEW' } }  }
-       )
-   );
-    setNewOrders(ordersData.data.listOrders.items);
-    }catch (e){
+        const ordersData = await API.graphql(
+          graphqlOperation(
+            listOrders,
+            { filter: { status: { eq: 'NEW'}}}
+            )
+        );
+        setNewOrders(ordersData.data.listOrders.items);
+    } catch (e) {
       console.log(e);
     }
   }
 
-   useEffect(()=>{
+  useEffect(() => {
     fetchCar();
     fetchOrders();
+  }, []);
 
-   }, []);
-
-
-   const onDecline=()=>{
+  const onDecline = () => {
     setNewOrders(newOrders.slice(1));
-   }
-   
-   const onAccept= async (newOrder)=>{
+  }
+
+  const onAccept = async (newOrder) => {
     try {
-    const input ={
-      id:newOrder.id,
-      status:"PICKING_UP_CLIENT",
-      carId:car.Id
-    }
-    const orderData =await API.graphql(
-      graphqlOperation(updateOrder , {input})
-    )
-    setOrder(orderData.data.updateOrder);
-    }catch (e){
-
-    }
-
-    
-    setNewOrders(newOrders.slice(1));
-
-   }
-
-   const onGoPress = async()=>{
-    // update the car and set it is online
-    try{
-      const userData = await Auth.currentAuthenticatedUser();
-      const input={
-        id:userData.attributes.sub,
-        isActive:!car.isActive,
+      const input = {
+        id: newOrder.id,
+        status: "PICKING_UP_CLIENT",
+        carId: car.id
       }
-      const updatedCarData =await API.graphql(
-        graphqlOperation(updateCar, {input})
+      const orderData = await API.graphql(
+        graphqlOperation(updateOrder, { input })
+      )
+      setOrder(orderData.data.updateOrder);
+    } catch (e) {
 
+    }
+
+    setNewOrders(newOrders.slice(1));
+  }
+
+  const onGoPress = async () => {
+    // Update the car and set it to active
+    try {
+      const userData = await Auth.currentAuthenticatedUser();
+      const input = {
+        id: userData.attributes.sub,
+        isActive: !car.isActive,
+      }
+      const updatedCarData = await API.graphql(
+        graphqlOperation(updateCar, { input })
       )
       setCar(updatedCarData.data.updateCar);
-    }catch (e){
+    } catch (e) {
       console.error(e);
     }
-   }
-   const onUserLocationChange=async(event)=>{
-  
-    const {latitude, longitude,heading}=event.nativeEvent.coordinate
-       // update the car and set it is online
-       try{
-        const userData = await Auth.currentAuthenticatedUser();
-        const input={
-          id:userData.attributes.sub,
-          latitude,
-          longitude,
-          heading ,
-        }
-        const updatedCarData =await API.graphql(
-          graphqlOperation(updateCar, {input})
-  
-        )
-        setCar(updatedCarData.data.updateCar);
-      }catch (e){
-        console.error(e);
+  }
+
+  const onUserLocationChange = async (event) => {
+    const { latitude, longitude, heading } = event.nativeEvent.coordinate
+    // Update the car and set it to active
+    try {
+      const userData = await Auth.currentAuthenticatedUser();
+      const input = {
+        id: userData.attributes.sub,
+        latitude,
+        longitude,
+        heading,
       }
-   }
-   const onDirectionFound=(event)=>{
-    console.log("Direction Found",event);
-    if (order){
+      const updatedCarData = await API.graphql(
+        graphqlOperation(updateCar, { input })
+      )
+      setCar(updatedCarData.data.updateCar);
+    } catch (e) {
+      console.error(e);
+    }
+  }
+
+  const onDirectionFound = (event) => {
+    console.log("Direction found: ", event);
+    if (order) {
       setOrder({
         ...order,
-        distance:  event.distance,
-        duration:  event.duration,
-        pickedUp:  order.pickedUp || event.distance <0.2,
-        isFinished: order.pickedUp && event.distance <0.2,
+        distance: event.distance,
+        duration: event.duration,
+        pickedUp: order.pickedUp || event.distance < 0.2,
+        isFinished: order.pickedUp && event.distance < 0.2,
       })
     }
-   }
+  }
 
- const getDestination =()=>{
-  if (order && order.pickedUp){
+  const getDestination = () => {
+    if (order && order.pickedUp) {
+      return {
+        latitude: order.destLatitude,
+        longitude: order.destLongitude,
+      }
+    }
     return {
-      latitude: order.destLatitude,
-      longitude: order.destLongitude,
+      latitude: order.originLatitude,
+      longitude: order.oreiginLongitude,
     }
   }
-  return{
-    latitude: order.originLatitude,
-    longitude: order.originLongitude,
-  }
- }
 
- const renderBottomTitle = () => {
-  if (order && order.isFinished) {
-    return (
-      <View style={{ alignItems: 'center' }}>
-        <View
-          style={{
-            flexDirection: 'row',
-            alignItems: 'center',
-            justifyContent: 'center',
-            backgroundColor: '#cb1a1a',
-            width: 200,
-            padding: 10,
-          }}>
-          <Text style={{color:'white',fontWeight:'bold'}}> Complete {order.type}</Text>
-        </View>
-        <Text style={styles.bottomText}>{order?.user?.username} </Text>
-      </View>
-    );
-  }
-
-  if (order && order.pickedUp) {
-    return (
-      <View style={{ alignItems: 'center' }}>
-        <View style={{ flexDirection: 'row', alignItems: 'center' }}>
-          <Text> {order.duration ? order.duration.toFixed(1) : '?'} </Text>
-          <View
-            style={{
-              backgroundColor: '#235536',
-              marginHorizontal: 10,
-              width: 30,
-              height: 30,
-              alignItems: 'center',
-              justifyContent: 'center',
-              borderRadius: 20,
-            }}>
-            <FontAwesome name={'user'} color={'Yellow'} size={20} />
+  const renderBottomTitle = () => {
+    if (order && order.isFinished) {
+      return (
+        <View style={{ alignItems: 'center' }}>
+          <View style={{flexDirection: 'row', alignItems: 'center',justifyContent: 'center', backgroundColor: '#cb1a1a', width: 200, padding: 10,  }}>
+            <Text style={{color: 'white', fontWeight: 'bold'}}>COMPLETE {order.type}</Text>
           </View>
-          <Text>{order.distance ? order.distance.toFixed(1) : '?'} Km</Text>
+          <Text style={styles.bottomText}>{order.user.username}</Text>
         </View>
-        <Text style={styles.bottomText}> Dropping off {order?.user?.username} </Text>
-      </View>
-    );
-  }
+      )
+    }
 
-  if (order) {
-    return (
-      <View style={{ alignItems: 'center' }}>
-        <View style={{ flexDirection: 'row', alignItems: 'center' }}>
-          <Text> {order.duration ? order.duration.toFixed(1) : '?'} </Text>
-          <View
-            style={{
-              backgroundColor: '#235536',
-              marginHorizontal: 10,
-              width: 30,
-              height: 30,
-              alignItems: 'center',
-              justifyContent: 'center',
-              borderRadius: 20,
-            }}>
-            <FontAwesome name={'user'} color={'white'} size={20} />
+    if (order && order.pickedUp) {
+      return (
+        <View style={{ alignItems: 'center' }}>
+          <View style={{flexDirection: 'row', alignItems: 'center'}}>
+            <Text>{order.duration ? order.duration.toFixed(1) : '?'} min</Text>
+            <View style={{ backgroundColor: '#235536', marginHorizontal: 10, width: 30, height: 30, alignItems:'center', justifyContent: 'center', borderRadius: 20}}>
+              <FontAwesome name={"user"} color={"Yellow"} size={20} />
+            </View>
+            <Text>{order.distance ? order.distance.toFixed(1) : '?'} km</Text>
           </View>
-          <Text>{order.distance ? order.distance.toFixed(1) : '?'} Km</Text>
+          <Text style={styles.bottomText}>Dropping off {order?.user?.username}</Text>
         </View>
-        <Text style={styles.bottomText}> Picking Up {order.user.name} </Text>
-      </View>
-    );
+      )
+    }
+
+    if (order) {
+      return (
+        <View style={{ alignItems: 'center' }}>
+          <View style={{flexDirection: 'row', alignItems: 'center'}}>
+            <Text>{order.duration ? order.duration.toFixed(1) : '?'} min</Text>
+            <View style={{ backgroundColor: '#235536', marginHorizontal: 10, width: 30, height: 30, alignItems:'center', justifyContent: 'center', borderRadius: 20}}>
+              <FontAwesome name={"user"} color={"white"} size={20} />
+            </View>
+            <Text>{order.distance ? order.distance.toFixed(1) : '?'} km</Text>
+          </View>
+          <Text style={styles.bottomText}>Picking up {order?.user?.username}</Text>
+        </View>
+      )
+    }
+    if (car?.isActive) {
+      return (
+        <Text style={styles.bottomText}>You're online</Text>
+      )
+    }
+    return (<Text style={styles.bottomText}>You're offline</Text>);
   }
 
-  if (car?.isActive) {
-    return <Text style={styles.bottomText}> You are Online </Text>;
-  }
-
-  return <Text style={styles.bottomText}> You are Offline </Text>;
-};
-
-   
   return (
     <View>
       <MapView
-        style={{width:'100%' ,height:Dimensions.get('window').height-150}}
-        provider={PROVIDER_GOOGLE} 
+        style={{width: '100%', height: Dimensions.get('window').height - 150}}
+        provider={PROVIDER_GOOGLE}
         showsUserLocation={true}
         onUserLocationChange={onUserLocationChange}
         initialRegion={{
@@ -239,27 +204,27 @@ const HomeScreen = () => {
           latitudeDelta: 0.025,
           longitudeDelta: 0.035,
         }}
-      > 
-      {order && (
-              <MapViewDirections
-                  origin={{
-                    latitude:car?.latitude,
-                    longitude:car?.longitude,
-                  }}
-                  onReady={onDirectionFound}
-                  destination={getDestination()}
-                  apikey={GOOGLE_MAPS_APIKEY}
-                  strokeWidth={9}
-                  strokeColor="black"
-              />
-      )}
+      >
+        {order && (
+          <MapViewDirections
+            origin={{
+              latitude: car?.latitude,
+              longitude: car?.longitude,
+            }}
+            onReady={onDirectionFound}
+            destination={getDestination()}
+            apikey={GOOGLE_MAPS_APIKEY}
+            strokeWidth={8}
+            strokeColor="black"
+          />
+        )}
+      </MapView>
 
-        </MapView>
-        <Pressable
+      <Pressable
         onPress={() => console.warn('Balance')}
         style={styles.balanceButton}>
         <Text style={styles.balanceText}>
-          <Text style={{ color: 'white' }}>Rs</Text>
+          <Text style={{ color: 'whit' }}>Rs</Text>
           {' '}
           0.00
         </Text>
@@ -293,22 +258,23 @@ const HomeScreen = () => {
         onPress={onGoPress}
         style={styles.goButton}>
         <Text style={styles.goText}>
-          {car?.isOnline ? 'END' :'GO'}
-          </Text>
-        </Pressable>
-        <View style={styles.bottomContainer}>
-          <FontAwesome name={"sliders"}size={30} color="#4a4a4a" />
+          {car?.isActive ? 'END' : 'GO'}
+        </Text>
+      </Pressable>
 
-               {renderBottomTitle()}
-          <Octicons name={"multi-select"} size={30} color="#4a4a4a"  />
-        </View>
-       {newOrders.length>0 &&!order && <NewOrderPopup 
+      <View style={styles.bottomContainer}>
+      <FontAwesome name={"sliders"}size={30} color="#4a4a4a" />
+          {renderBottomTitle()}
+          <Octicons name={"multi-select"} size={30} color="#4a4a4a" />
+      </View>
+
+      {newOrders.length > 0 && !order && <NewOrderPopup
         newOrder={newOrders[0]}
         duration={2}
         distance={0.5}
         onDecline={onDecline}
-        onAccept={()=>onAccept(newOrders[0])}
-        />}
+        onAccept={() => onAccept(newOrders[0])}
+      />}
     </View>
   );
 };
